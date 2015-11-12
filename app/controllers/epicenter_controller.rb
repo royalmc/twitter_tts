7c85b2f5845f062829d5d7435d962d4971159416
Royal <iroyal@gmail.com>
2015-11-11 20:59:56 -0500

    cleaner code

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -2,49 +2,48 @@ class EpicenterController < ApplicationController
   def feed
 
 
-
-
   	@following_tweets = []
 
 
   	@tweets = Tweet.all
 
 
-
-
-
-  	@tweets.each do |tweet|
+    @tweets.each do |tweet|
   		current_user.following.each do |f|
   			if tweet.user_id == f
   				@following_tweets.push(tweet)
-
-
-
   			end
   		end
   	end
   end	
 
   def show_user
-    @user = User.find(params [:id])
+
+
+    @user = User.find(params[:id])
   end
 
   def now_following
     # This line is just for displaying purposes:
     @user = User.find(params[:follow_id])
 
-
-
-
     current_user.following.push(params[:follow_id].to_i)
 
-
-
-
     current_user.save
-
   end
 
   def unfollow
+
+    @user = User.find(params[:unfollow_id])
+
+    current_user.following.delete(params[:unfollow_id].to_i)
+
+    current_user.save
   end
-end
+
+  def js_practice
+
+
+  end 
+end  
+
