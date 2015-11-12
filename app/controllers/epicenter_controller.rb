3a97c2a4d5fb1b1c2da758ea686d63dcd0d6aa7b
Royal <iroyal@gmail.com>
2015-11-11 19:17:31 -0500

    first commit

---
+++ app/controllers/epicenter_controller.rb
@@ -0,0 +1,50 @@
+class EpicenterController < ApplicationController
+  def feed
+
+
+
+
+  	@following_tweets = []
+
+
+  	@tweets = Tweet.all
+
+
+
+
+
+  	@tweets.each do |tweet|
+  		current_user.following.each do |f|
+  			if tweet.user_id == f
+  				@following_tweets.push(tweet)
+
+
+
+  			end
+  		end
+  	end
+  end	
+
+  def show_user
+    @user = User.find(params [:id])
+  end
+
+  def now_following
+    # This line is just for displaying purposes:
+    @user = User.find(params[:follow_id])
+
+
+
+
+    current_user.following.push(params[:follow_id].to_i)
+
+
+
+
+    current_user.save
+
+  end
+
+  def unfollow
+  end
+end
